f3c8d0e32ef044ae7da01f8b4e01144b619cc0ef
Mateusz Borycki <mateuszborycki@gmail.com>
2023-04-01 22:52:01 +0200

    Remove test things.

--- src/sample.ts
+++ src/sample.ts
@@ -1,11 +1,6 @@
-import {checkoutRepo, getChangeset} from './plastic'
-
 async function run(): Promise<void> {
-  process.chdir('V:/aig/wkspaces/The_Darkest_Ballad')
-  //await checkoutRepo('TestPlastic@ShrineOfChaos@cloud')
-  const cs = await getChangeset()
   // eslint-disable-next-line no-console
-  console.log(cs)
+  console.log('Test')
 }
 
 run()
